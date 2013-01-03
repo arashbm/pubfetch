feeb509700fb6859e6bcfa5910f043edceb56e4f
Arash Badie Modiri <arashbm@gmail.com>
2013-01-03 12:02:26 +0330

    now getting args via cli

--- pubfetch.rb
+++ pubfetch.rb
@@ -1,9 +1,21 @@
+#!/usr/bin/env ruby
 require 'nokogiri'
 require 'net/http'
 require 'open-uri'
+if ARGV.length < 2
+  puts 'Usage: pubfetch [start] [finish]
 
-ids=1_000_000...1_050_000
-chunks=1000
+Downloads articles from PubMed in chunks of 1000 as XML. From id [start] to [finish]'
+  raise ArgumentError
+end
+
+
+start = ARGV[0].to_i
+finish = ARGV[1].to_i
+ids = start...finish
+chunks = 1000
+
+puts "Trying to register a query for ids within range '#{ids.to_s}'"
 
 epost_query = { db: 'pubmed', id: ids.to_a.join(',') }
 epost_url= URI.parse 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi'
@@ -16,6 +28,10 @@ puts "Got a query_key (#{query_key}) and WebEnv (#{webenv})"
 
 ids.each_slice(chunks) do |s|
   start = ids.min-s.min
+
+  puts "="*80
+  puts "Downloading a chunk of #{chunks} articles from #{s.min} to #{s.max}..."
+
   system "wget --output-document pub-#{s.min}-#{s.max}.xml 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=#{query_key}&WebEnv=#{webenv}&retmax=#{chunks}&retstart=#{start}&retmode=xml'"
   sleep 20
 end

3190e82839157b56d0506b12d9488dbaf1bdac95
Arash Badie Modiri <arashbm@gmail.com>
2013-09-18 15:51:07 +0430

    fixed some mem consumption issues

--- pubfetch.rb
+++ pubfetch.rb
@@ -2,6 +2,22 @@
 require 'nokogiri'
 require 'net/http'
 require 'open-uri'
+
+
+def get_query_id(ids)
+  puts "Trying to register a query for ids within range '#{ids.to_s}'"
+
+  epost_query = { db: 'pubmed', id: ids.to_a.join(',') }
+  epost_url= URI.parse 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi'
+
+  epost_response = Net::HTTP.post_form(epost_url, epost_query)
+  epost_doc = Nokogiri::XML(epost_response.body)
+  query_key = epost_doc.css('QueryKey').text
+  webenv= epost_doc.css('WebEnv').text
+  puts "Got a query_key (#{query_key}) and WebEnv (#{webenv})"
+  return [query_key, webenv]
+end
+
 if ARGV.length != 2
   puts 'Usage: pubfetch [start] [finish]
 
@@ -13,18 +29,9 @@ end
 start = ARGV[0].to_i
 finish = ARGV[1].to_i
 ids = start...finish
-chunks = 500
+chunks = 100000
 
-puts "Trying to register a query for ids within range '#{ids.to_s}'"
-
-epost_query = { db: 'pubmed', id: ids.to_a.join(',') }
-epost_url= URI.parse 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi'
-
-epost_response = Net::HTTP.post_form(epost_url, epost_query)
-epost_doc = Nokogiri::XML(epost_response.body)
-query_key = epost_doc.css('QueryKey').text
-webenv= epost_doc.css('WebEnv').text
-puts "Got a query_key (#{query_key}) and WebEnv (#{webenv})"
+query_key, webenv = get_query_id(ids)
 
 ids.each_slice(chunks) do |s|
   start = s.min-ids.min
@@ -34,6 +41,7 @@ ids.each_slice(chunks) do |s|
 
   targetfile = "pub-#{s.min}-#{s.max}.xml"
   begin
+    GC.start #manually triggering garbage collection
     system "wget --no-verbose --output-document #{targetfile} 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=#{query_key}&WebEnv=#{webenv}&retmax=#{chunks}&retstart=#{start}&retmode=xml'"
     doc = Nokogiri.XML(File.open targetfile)
     sleep 5
